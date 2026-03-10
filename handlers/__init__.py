7f4ae63a21093bce0fffea07554412e86eef8d7c
justyaviz <justyaviz@gmail.com>
2026-03-10 07:10:15 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,1 @@
-from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(CommandStart())
-async def start(message: Message):
-    await message.answer("Bot ishga tushdi 🚀")
+# handlers package
